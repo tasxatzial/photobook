bcd4c9fc0e02662d168ef174078b33aff24232a1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2019-12-02 21:24:38 +0200

    add a function that requests the signin page

--- logbook/src/main/webapp/js/landing.js
+++ logbook/src/main/webapp/js/landing.js
@@ -8,7 +8,32 @@ var Landing = (function() {
   };
 
   function showSignin() {
+    var state = {
+      xhr: null
+    };
 
+    /* disable signin button to avoid clicking it again */
+    el.landingSigninButton.disabled =  true;
+
+    /* get the signin page, prepare the data */
+    var data = new FormData();
+    data.append('action', 'GetSignin');
+
+    /* make the call to the main servlet */
+    state.xhr = ajaxRequest('POST', 'Main', data, successCallback, failCallback);
+
+    function successCallback() {
+
+      /* replace the content of index with the signin page */
+      el.nonav.innerHTML = state.xhr.responseText;
+    }
+
+    function failCallback() {
+
+      /* getting the signin page was a fail, so we need to re-enable the signin button */
+      el.landingSigninButton.disabled =  false;
+      console.log(state.xhr.responseText);
+    }
   }
 
   function showSignup() {
