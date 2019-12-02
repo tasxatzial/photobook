9f7764ff3ca427cbf39b902e5cf4ad7a8068f3ca
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2019-12-03 01:10:41 +0200

    add a function that shows a specific page from the user list

--- logbook/src/main/webapp/js/all_users.js
+++ logbook/src/main/webapp/js/all_users.js
@@ -1,7 +1,24 @@
 var ShowAllUsers = (function() {
+  var state = {
+    pages: 1
+  };
+
+  var el = {
+    userListParent: null,
+  };
 
   function init() {
 
+  }
+
+  function showPage(pageNo) {
+    if (pageNo <= state.pages && pageNo >= 1) {
+      if (el.userListParent.children[0].children[1]) {
+        el.userListParent.children[0].removeChild(el.userListParent.children[0].children[1]);
+      }
+      var userPage = newElements.createAccountDetails(state.xhrResponse[pageNo], null, false);
+      el.userListParent.children[0].appendChild(userPage);
+    }
   }
 
   return {
