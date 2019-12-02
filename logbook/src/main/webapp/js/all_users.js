66063df18669b0506f518ca8c09650f75072809d
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2019-12-03 01:11:36 +0200

    add listeners to left/right buttons for the user list navigation

--- logbook/src/main/webapp/js/all_users.js
+++ logbook/src/main/webapp/js/all_users.js
@@ -5,10 +5,44 @@ var ShowAllUsers = (function() {
 
   var el = {
     userListParent: null,
+    nonav: null
   };
 
   function init() {
 
+  }
+
+  function addListeners() {
+    var navButtons = el.userListParent.children[1];
+    var leftButton = navButtons.children[0];
+    var selectButton = navButtons.children[1].children[0];
+    var rightButton = navButtons.children[2];
+
+    leftButton.disabled = true;
+    if (state.pages <= 1) {
+      rightButton.disabled = true;
+    }
+    leftButton.addEventListener('click', function () {
+      selectButton.value = Number(selectButton.value) - 1;
+      if (selectButton.value == 1) {
+        leftButton.disabled = true;
+      }
+      rightButton.disabled = false;
+      showPage(selectButton.value);
+    });
+    rightButton.addEventListener('click', function () {
+      selectButton.value = Number(selectButton.value) + 1;
+      if (selectButton.value == state.pages) {
+        rightButton.disabled = true;
+      }
+      leftButton.disabled = false;
+      showPage(selectButton.value);
+    });
+    selectButton.addEventListener('change', function () {
+      showPage(selectButton.value);
+      leftButton.disabled = selectButton.value == 1;
+      rightButton.disabled = selectButton.value == state.pages;
+    });
   }
 
   function showPage(pageNo) {
