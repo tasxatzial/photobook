a1e581766c61de35a8909967c2b9ce356fae2afa
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2019-12-02 20:56:59 +0200

    add ajax call for landing page

---
+++ logbook/src/main/webapp/js/init.js
@@ -0,0 +1,24 @@
+(function() {
+  var state = {
+    xhr: null
+  };
+
+  var data = new FormData();
+  data.append("action", "GetLanding");
+  state.xhr = ajaxRequest('POST', 'Main', data, successCallback, failCallback);
+
+  function successCallback() {
+    document.getElementById('no-nav').innerHTML = state.xhr.responseText;
+    var id = document.getElementById('landing-section');
+    if (id) {
+      /* Landing.init(); */
+    }
+    else {
+
+    }
+  }
+
+  function failCallback() {
+    console.log(state.xhr.responseText);
+  }
+}());
