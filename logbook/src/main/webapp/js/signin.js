60e63d1dc2654cb74b06dfa3e3ae5a9c29655070
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2019-12-03 00:35:27 +0200

    add signin function

--- logbook/src/main/webapp/js/signin.js
+++ logbook/src/main/webapp/js/signin.js
@@ -21,6 +21,49 @@ var Signin = (function() {
     }
   }
 
+  function doSignin() {
+    var state = {
+      xhr: null
+    };
+
+    disableInputs(true);
+
+    /* prepare data */
+    var data = new FormData();
+    data.append(el.username.name.split('-')[1], el.username.value);
+    data.append(el.password.name.split('-')[1], el.password.value);
+    data.append('action', 'Signin');
+
+    /* make the call */
+    state.xhr = ajaxRequest('POST', 'Main', data, successCallback, failCallback);
+
+    function successCallback() {
+      if (state.xhr.getResponseHeader("content-type").split(';')[0] === 'application/json') {
+        var response = JSON.parse(state.xhr.responseText);
+        var invalid = null;
+        if (response['username'] === '0') {
+          invalid = 'username';
+        }
+        else {
+          invalid = 'password';
+        }
+        el.signinMsg.innerHTML = 'Invalid ' + invalid;
+        el.signinMsg.style.color = 'red';
+        disableInputs(false);
+      }
+      else {
+        el.nonav.innerHTML = state.xhr.responseText;
+        el.navbarContent.removeChild(el.signupButton);
+        /* Homepage.init(); */
+      }
+    }
+
+    function failCallback() {
+      disableInputs(false);
+      console.log(state.xhr.responseText);
+    }
+  }
+
   function init() {
     el.signinButton = document.querySelector('#signin-button input');
     el.username = document.getElementById('signin-username');
@@ -34,9 +77,7 @@ var Signin = (function() {
     el.username.addEventListener('input', function() {el.signinMsg.innerHTML = '';});
     el.password.addEventListener('input', function() {el.signinMsg.innerHTML = '';});
     el.signinButton.addEventListener('click', function() {el.signinMsg.innerHTML = '';});
-    el.signinButton.addEventListener('click', function () {
-
-    });
+    el.signinButton.addEventListener('click', doSignin);
     el.signupButton.addEventListener('click', function () {
       el.signupButton.disabled = true;
       Landing.showSignup();
