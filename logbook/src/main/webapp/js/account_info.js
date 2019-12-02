97b317134ee3fcb6f2d6edbc29b543fee0466221
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2019-12-03 01:30:02 +0200

    add callbacks for the ajax call for the account info

--- logbook/src/main/webapp/js/account_info.js
+++ logbook/src/main/webapp/js/account_info.js
@@ -19,11 +19,35 @@ var ShowAccountInfo = (function() {
     state.xhr = ajaxRequest("POST", "Main", data, successCallback, failCallback);
 
     function successCallback() {
+      var nonav = document.getElementById('no-nav');
+      nonav.innerHTML = state.xhr.responseText;
+      nonav.style.height = 'auto';
+      formInput.disable(document.getElementById('signup-username'));
 
+      var countryHidden = document.getElementById('country-hidden');
+      var country = document.getElementById('signup-country');
+      country.children[0].selected = 'false';
+      for (var j = 0; j < country.children.length; j++) {
+        if (country.children[j].value === countryHidden.innerHTML) {
+          country.children[j].selected = 'true';
+          break;
+        }
+      }
+
+      var genderHidden = document.getElementById('gender-hidden');
+      var gender = document.querySelectorAll('input[type="radio"]');
+      for (var i = 0; i < gender.length; i++) {
+        if (gender[i].value === genderHidden.innerHTML) {
+          gender[i].checked = 'true';
+        }
+      }
+      el.accountInfoButton.disabled = false;
+      Signup.init('UpdateAccount');
     }
 
     function failCallback() {
-      
+      el.accountInfoButton.disabled = false;
+      console.log(state.xhr.responseText);
     }
   }
 
