6a48445f56b4b4cff558eedbd8386af2a3773c56
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2020-11-26 15:37:23 +0200

    deal with the errors on the client side when a rating a post fails

--- logbook/src/main/webapp/js/posts.js
+++ logbook/src/main/webapp/js/posts.js
@@ -266,15 +266,6 @@ var Posts = (function() {
       else if (Requests.get(ID).status === 500) {
         error = 'Server error';
       }
-      else if (Requests.get(ID).status === 400) {
-        responseText = Requests.get(ID).responseText;
-        if (!responseText) {
-          error = 'Error';
-        }
-        else {
-          error = 'Invalid post';
-        }
-      }
       else if (Requests.get(ID).status === 0) {
         error = 'Unable to send request';
       }
@@ -629,6 +620,7 @@ var Posts = (function() {
         ratePost(selectRate.value, data);
     });
     data['ratingTextDiv'].parentElement.appendChild(selectRate);
+    data['selectRateDiv'] = selectRate;
 
     el.postsParent.innerHTML = '';
     el.postsParent.appendChild(data['postDiv']);
@@ -642,21 +634,57 @@ var Posts = (function() {
      * @param data
      */
   function ratePost(rate, data) {
+      data['selectRateDiv'].disabled = true;
+
       var formData = new FormData();
       formData.append("action", "RatePost");
       formData.append("postID", data['postID']);
       formData.append("rate", rate);
 
+      var oldRatingText = data['ratingTextDiv'].innerHTML;
+
+      data['ratingTextDiv'].innerHTML = '';
+      data['ratingTextDiv'].appendChild(Init.loader);
+
       var ID = Requests.add(ajaxRequest('POST', "Main", formData, successCallback, failCallback));
 
       function successCallback() {
+        data['selectRateDiv'].disabled = false;
+        data['userRating'] = rate;
         var response = JSON.parse(Requests.get(ID).responseText);
         data['ratingTextDiv'].innerHTML = createRatingsText(response['ratings']);
       }
 
       function failCallback() {
-        var response = JSON.parse(Requests.get(ID).responseText);
-        console.log(response);
+        var selectRate = document.querySelector('.select-rate');
+        if (!selectRate) {
+          return;
+        }
+        data['selectRateDiv'].disabled = false;
+        data['selectRateDiv'].children[data['userRating']].selected = true;
+        data['ratingTextDiv'].innerHTML = oldRatingText;
+
+        var error = null;
+        if (Requests.get(ID).status === 401) {
+          var responseText = Requests.get(ID).responseText;
+          if (!responseText) {
+            error = 'Error';
+          }
+          else if (JSON.parse(responseText).ERROR === 'NO_SESSION') {
+            Logout.showExpired();
+            return;
+          }
+        }
+        else if (Requests.get(ID).status === 500) {
+          error = 'Rating failed: Server error';
+        }
+        else if (Requests.get(ID).status === 0) {
+          error = 'Rating failed: Unable to send request';
+        }
+        else {
+          error = 'Rating failed: Error';
+        }
+        newElements.showFullWindowMsg('OK', error, Init.clearFullWindowMsg);
       }
   }
 
